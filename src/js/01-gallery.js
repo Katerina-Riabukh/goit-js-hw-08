95b6e341bed9a4c93efaa000cacd4dce06123411
Katrysya7773 <121060967+Katrysya7773@users.noreply.github.com>
2023-05-14 20:38:28 +0200

    1 - 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from "simplelightbox/src/simple-lightbox";
+console.log(SimpleLightbox);
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector('.gallery');
+
+const listMarcap = galleryItems.map(({ preview, original, description }) => {
+
+    return ` <li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img class="gallery__image"
+                    src="${preview}" 
+                    alt="${description}" />
+                </a>
+            </li>`
+   
+}).join('');
+
+list.insertAdjacentHTML("afterbegin", listMarcap);
+
+const lightbox = new SimpleLightbox('.gallery a', { animationSpeed: 150, captionsData: 'alt', captionDelay: 250, });
+ 
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,57 @@
+ import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+//ctrl + alt + l ;
 
+const iframe = document.getElementById('vimeo-player')
+const player = new Player(iframe);
+//const player = new Player("vimeo-player", "https://player.vimeo.com/video/236203659");
+
+const onPlay = function({seconds}) {
+    localStorage.setItem('videoplayer-current-time', seconds);      
+};
+
+const currentSeconds = localStorage.getItem('videoplayer-current-time')
+// console.log( currentSeconds)
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(currentSeconds).then(function (currentSeconds) {
+  
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            //localStorage.removeItem('videoplayer-current-time')
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+
+
+ //if (currentTime === duration) {
+//       localStorage.removeItem('videoplayer-current-time')
+//   }
+
+
+// player.on('timeupdate', throttle(onPlay, 1000));
+
+
+// // player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+
+// // import throttle from 'lodash.throttle';
+
+// // const iframe = document.querySelector('iframe');
+// // const player = new Vimeo.Player(iframe);
+
+// // player.on('timeupdate', throttle(onPlay, 1000));
+
+// // function onPlay({ seconds }) {
+// //   localStorage.setItem('videoplayer-current-time', seconds);
+// // }
+
+// // player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
