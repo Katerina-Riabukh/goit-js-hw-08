2110ac20c8edd25a7626931e7a33abeef84ed0f2
Katrysya7773 <121060967+Katrysya7773@users.noreply.github.com>
2023-05-17 12:08:52 +0200

    final example

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,54 +4,37 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.getElementById('vimeo-player')
 const player = new Player(iframe);
-//const player = new Player("vimeo-player", "https://player.vimeo.com/video/236203659");
+const currentSeconds = localStorage.getItem('videoplayer-current-time')
+const duration = localStorage.getItem('videoplayer-duration')
+console.log(duration);
+console.log(currentSeconds);
 
-const onPlay = function({seconds}) {
-    localStorage.setItem('videoplayer-current-time', seconds);      
+const onPlay = function ({ seconds, duration}) {
+
+    localStorage.setItem('videoplayer-current-time', seconds , duration); 
+    localStorage.setItem('videoplayer-duration', duration);
+    // if (currentSeconds === duration) {
+    //     localStorage.removeItem('videoplayer-current-time')
+    // }
 };
 
-const currentSeconds = localStorage.getItem('videoplayer-current-time')
-// console.log( currentSeconds)
+
+console.log(localStorage);
+
+
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
 player.setCurrentTime(currentSeconds).then(function (currentSeconds) {
-  
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            //localStorage.removeItem('videoplayer-current-time')
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
+ 
 });
 
 
 
- //if (currentTime === duration) {
-//       localStorage.removeItem('videoplayer-current-time')
-//   }
 
 
-// player.on('timeupdate', throttle(onPlay, 1000));
-
-
-// // player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-
-
-// // import throttle from 'lodash.throttle';
-
-// // const iframe = document.querySelector('iframe');
-// // const player = new Vimeo.Player(iframe);
-
-// // player.on('timeupdate', throttle(onPlay, 1000));
-
-// // function onPlay({ seconds }) {
-// //   localStorage.setItem('videoplayer-current-time', seconds);
-// // }
-
-// // player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+// {
+//     duration: 61.857
+//     percent: 1
+//     seconds: 61.857
+// }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector(".feedback-form")
+const textArea = document.querySelector('textarea')
+const email = document.querySelector('input')
+const dataForm = {
+    email: '',
+    message: ''
+}
+
+form.addEventListener('click', throttle(onFillInputs, 500))
+form.addEventListener('submit', onFormSubmit)
+
+const returnData = JSON.parse(localStorage.getItem('feedback-form-state')) || '';
+
+returnFieldValue()
+
+function onFillInputs(event) {
+   
+    const email = event.currentTarget.elements.email.value
+    const message = event.currentTarget.elements.message.value
+   
+    dataForm.email = email;
+    dataForm.message = message
+    localStorage.setItem('feedback-form-state', JSON.stringify(dataForm))
+}
+
+
+function returnFieldValue() {
+    if (returnData) {
+        textArea.value = returnData.message;
+        email.value = returnData.email
+    }
+}
+
+function onFormSubmit(event) {
+    event.preventDefault()
+
+ if (email.value === "" || textArea.value === "") {
+        alert('Pleas fill in all required fields')
+        return
+    }
+
+    form.reset()
+    localStorage.removeItem("feedback-form-state")
+}
 
